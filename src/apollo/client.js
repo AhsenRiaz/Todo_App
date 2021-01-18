6412b405961bbb34aded8911e8528be75e03b624
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-01-18 19:53:31 +0500

    Updated File

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import fetch from "cross-fetch";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:8888/.netlify/functions/todoList",
+    uri: ".netlify/functions/todoList",
     fetch
   }),
   
